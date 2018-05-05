b6a43ada338565ab6681fb525e1f05a4f165a07a
mohammadsalkini <eng.salkini@hotmail.com>
2018-05-05 19:53:39 +0200

    Add validation

--- eMusicStore/src/main/java/com/emusicstore/controller/HomeController.java
+++ eMusicStore/src/main/java/com/emusicstore/controller/HomeController.java
@@ -5,13 +5,16 @@ import com.emusicstore.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -76,7 +79,11 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/admin/productInventory/addProduct", method = RequestMethod.POST)
-    public String addProductPost(@ModelAttribute("product") Product product, HttpServletRequest request) {
+    public String addProductPost(@Valid @ModelAttribute("product") Product product, BindingResult result, HttpServletRequest request) {
+
+        if (result.hasErrors()) {
+            return "addProduct";
+        }
         productDao.addProduct(product);
 
         MultipartFile productImage = product.getProductImage();
@@ -126,8 +133,12 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/admin/productInventory/editProduct", method = RequestMethod.POST)
-    public String editProduct(@ModelAttribute("product") Product product, Model model, HttpServletRequest
+    public String editProduct(@Valid @ModelAttribute("product") Product product, Model model,BindingResult result, HttpServletRequest
             request) {
+
+        if (result.hasErrors()) {
+            return "editProduct";
+        }
 
         MultipartFile productImage = product.getProductImage();
         String rootDirectory = request.getSession().getServletContext().getRealPath("/");
--- eMusicStore/src/main/java/com/emusicstore/model/Product.java
+++ eMusicStore/src/main/java/com/emusicstore/model/Product.java
@@ -2,9 +2,11 @@ package com.emusicstore.model;
 
 
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 
 @Entity
 public class Product {
@@ -12,12 +14,18 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private String productId;
+
+    @NotEmpty (message = "The product name must not be null.")
     private String productName;
     private String productCategory;
     private String productDescription;
+
+    @Min(value = 0, message = "The product price must not be less than zero.")
     private double productPrice;
     private String productCondition;
     private String productStatus;
+
+    @Min(value = 0, message = "The product unit must not be less than zero.")
     private int unitInStock;
     private String productManufacture;
 
